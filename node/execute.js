059d492d8d230eef8a6810ceabfc530e6d3547ca
writersblockchain <seanmconrad94@gmail.com>
2024-02-13 12:41:26 -0500

    execute

--- node/execute.js
+++ node/execute.js
@@ -17,12 +17,19 @@ let codeHash =
   "6c0281401b31d5af254bf9291ea1a2aa90da5cb5074c4eb06bf39447fdd66915";
 let contractAddress = "secret1wl0srn0x8rkddf4grsfz7hfeapflchg62uw4x9";
 
-let query_contract = async () => {
-  let my_query = await secretjs.query.compute.queryContract({
-    contract_address: contractAddress,
-    code_hash: codeHash,
-    query: { get_count: {} },
-  });
-  console.log("emlanis' starting count: ", my_query);
+let try_execute = async () => {
+  const tx = await secretjs.tx.compute.executeContract(
+    {
+      sender: wallet.address,
+      contract_address: contractAddress,
+      msg: {
+        increment: {},
+      },
+      code_hash: codeHash,
+    },
+    { gasLimit: 100_000 }
+  );
+
+  console.log(tx);
 };
-query_contract();
+try_execute();
